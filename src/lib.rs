6a46d19627243ab7a113766b365dd1ec66f83235
David Beaumont <david.beaumont@birdie.care>
2021-12-20 18:24:07 +0000

    Upgrade to rust 2021

--- src/lib.rs
+++ src/lib.rs
@@ -14,11 +14,23 @@ pub fn get_occurrences(
 mod tests {
     use chrono::prelude::DateTime;
     use fhir_rs::model::Timing::TimingBuilder;
+    use fhir_rs::model::Timing_Repeat::{Timing_RepeatBuilder, Timing_RepeatPeriodUnit};
+    use fhir_rs::model::Period::PeriodBuilder;
 
     #[test]
     fn it_works() {
-        let timing_builder = TimingBuilder::new();
+        let mut repeat_builder = Timing_RepeatBuilder::new();
+        repeat_builder
+            // ._when(vec![ElementBuilder::new().build()])
+            .frequency(3)
+            .period(1f64)
+            .period_unit(Timing_RepeatPeriodUnit::D)
+            .bounds_period(PeriodBuilder::new().start("2021-06-21").build());
+
+        let mut timing_builder = TimingBuilder::new();
+        timing_builder.repeat(repeat_builder.build());
         let timing = timing_builder.build();
+
         let window_start = DateTime::parse_from_rfc3339("2021-06-21T00:00:00+01:00").unwrap();
         let window_finish = DateTime::parse_from_rfc3339("2021-06-22T00:00:00+01:00").unwrap();
 
