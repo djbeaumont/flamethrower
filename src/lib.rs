d185fde42963ace10c8c8b4a4512a1fc3a9e59b2
David Beaumont <david@zorq.net>
2025-10-17 09:12:00 +0100

    feat: bind function to wasm to allow projecting arbitrary fhir timing

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use chrono::NaiveDate;
+use chrono::{NaiveDate, NaiveTime};
+use wasm_bindgen::JsValue;
 use wasm_bindgen::prelude::wasm_bindgen;
 use crate::interval::Interval;
 use crate::timing::{FhirPeriod, FhirTiming, FhirTimingRepeat};
@@ -46,3 +47,70 @@ pub fn between(begin: &str, end: &str) -> Vec<String> {
 
     occurrences.iter().map(|o| o.to_string()).collect()
 }
+
+#[wasm_bindgen]
+pub fn project_occurrences(raw_timing: JsValue, start: &str, end: &str) -> Vec<String> {
+    let timing: FhirTiming = serde_wasm_bindgen::from_value(raw_timing).unwrap();
+
+    // let timing = FhirTiming {
+    //     repeat: FhirTimingRepeat {
+    //         bounds: FhirPeriod {
+    //             start: Some(NaiveDate::from_ymd_opt(2021, 6, 21).unwrap()),
+    //             end: None,
+    //         },
+    //         frequency: 1,
+    //         period: 1,
+    //         period_unit: "d".to_string(),
+    //         day_of_week: vec![],
+    //         time_of_day: vec![],
+    //         when: vec![],
+    //     },
+    // };
+
+    let interval = Interval {
+        start: &NaiveDate::parse_from_str(start, "%Y-%m-%d").unwrap().and_hms_opt(0, 0, 0).unwrap(),
+        end: Some(&NaiveDate::parse_from_str(end, "%Y-%m-%d").unwrap().and_hms_opt(0, 0, 0).unwrap()),
+    };
+    let occurrences = recurrence::between(&timing, &interval);
+    occurrences.iter().map(|o| o.to_string()).collect()
+}
+
+// #[wasm_bindgen]
+// pub fn project_occurrences(timing: &SerializableFhirTiming, start: &str, end: &str) -> Vec<String> {
+//     let interval = Interval {
+//         start: &NaiveDate::parse_from_str(start, "%Y-%m-%d").unwrap().and_hms_opt(0, 0, 0).unwrap(),
+//         end: Some(&NaiveDate::parse_from_str(end, "%Y-%m-%d").unwrap().and_hms_opt(0, 0, 0).unwrap()),
+//     };
+//     let parsed_timing = parse_serializable_timing(timing);
+//     let occurrences = recurrence::between(&parsed_timing, &interval);
+//     occurrences.iter().map(|o| o.to_string()).collect()
+// }
+//
+// fn parse_serializable_timing(timing: &SerializableFhirTiming) -> FhirTiming {
+//     let bounds = FhirPeriod {
+//         start: match &timing.repeat.bounds.start {
+//             Some(s) => Some(NaiveDate::parse_from_str(s, "%Y-%m-%d").unwrap()),
+//             None => None,
+//         },
+//         end: match &timing.repeat.bounds.end {
+//             Some(e) => Some(NaiveDate::parse_from_str(e, "%Y-%m-%d").unwrap()),
+//             None => None,
+//         },
+//     };
+//
+//     let time_of_day = timing.repeat.time_of_day.iter().map(|t| {
+//         NaiveTime::parse_from_str(t, "%H:%M:%S").unwrap()
+//     }).collect();
+//
+//     FhirTiming {
+//         repeat: FhirTimingRepeat {
+//             bounds,
+//             frequency: timing.repeat.frequency,
+//             period: timing.repeat.period,
+//             period_unit: timing.repeat.period_unit.clone(),
+//             day_of_week: timing.repeat.day_of_week.clone(),
+//             time_of_day,
+//             when: timing.repeat.when.clone(),
+//         },
+//     }
+// }
--- src/timing.rs
+++ src/timing.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
 use chrono::{NaiveDate, NaiveTime};
+use wasm_bindgen::prelude::wasm_bindgen;
 
 #[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
 pub struct FhirPeriod {
@@ -29,3 +30,30 @@ pub struct Occurrence {
     pub time_of_day: Option<NaiveTime>,
     pub when: Option<String>,
 }
+
+// TODO - work out how to remove this duplication by binding the structs above to wasm
+
+// #[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
+// #[wasm_bindgen(getter_with_clone)]
+// pub struct SerializableFhirPeriod {
+//     pub start: Option<String>,
+//     pub end: Option<String>,
+// }
+//
+// #[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
+// #[wasm_bindgen(getter_with_clone)]
+// pub struct SerializableFhirTimingRepeat {
+//     pub bounds: SerializableFhirPeriod,
+//     pub frequency: i64,
+//     pub period: i64,
+//     pub period_unit: String,
+//     pub day_of_week: Vec<String>,
+//     pub time_of_day: Vec<String>,
+//     pub when: Vec<String>,
+// }
+//
+// #[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
+// #[wasm_bindgen(getter_with_clone)]
+// pub struct SerializableFhirTiming {
+//     pub repeat: SerializableFhirTimingRepeat,
+// }
